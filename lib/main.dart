bdb2422b6f2d13b0b4a4fa1269f45982e09223bd
mahmoud7299 <mahmoud7299>
2022-07-23 11:07:22 +0200

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -24,13 +24,13 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const calcPage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
+class calcPage extends StatefulWidget {
+  const calcPage({Key? key, required this.title}) : super(key: key);
 
   // This widget is the home page of your application. It is stateful, meaning
   // that it has a State object (defined below) that contains fields that affect
@@ -44,10 +44,10 @@ class MyHomePage extends StatefulWidget {
   final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<calcPage> createState() => _calcPageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _calcPageState extends State<calcPage> {
   int _counter = 0;
 
   void _incrementCounter() {
